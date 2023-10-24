83f6b22d9c33a9c49eda412dafdc9ec9fbabe771
asilva-bruno <49911992+asilva-bruno@users.noreply.github.com>
2023-10-23 23:20:00 -0300

    app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -12,10 +12,24 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json( repositories );
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  
+  repositories.push( repository );
+
+  return response.json( repository );
+
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -24,6 +38,19 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex( 
+    repository => repository.id == id );
+
+  if( findRepositoryIndex >= 0 ){
+    repositories.splice( findRepositoryIndex, 1 )
+  }
+  else {
+    return response.status( 400 ).json( {error: 'Repositório inexistente!'} );
+  }
+  
+  return response.status( 204 ).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
